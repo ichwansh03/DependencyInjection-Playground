f7de37031deeb6e2a45985bb9ee893877243a172
ichwansh03 <ichwansholihin03@gmail.com>
2023-09-11 20:35:05 +0700

    chore: register AppModule

---
+++ app/src/main/java/com/ichwan/arch/designpattern/hilt/AppModule.kt
@@ -0,0 +1,19 @@
+package com.ichwan.arch.designpattern.hilt
+
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+import javax.inject.Named
+import javax.inject.Singleton
+
+@Module
+@InstallIn(SingletonComponent::class)
+object AppModule {
+
+    @Singleton
+    @Provides
+    @Named("string1")
+    fun provideString1() = "This is a string we will inject from string1"
+
+}
