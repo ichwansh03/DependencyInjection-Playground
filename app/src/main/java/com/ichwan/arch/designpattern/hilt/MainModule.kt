638b1cd1320cd937ec59f2f4332d32254429e57d
ichwansh03 <ichwansholihin03@gmail.com>
2023-09-12 06:09:54 +0700

    fixed: call view model and change scope

--- app/src/main/java/com/ichwan/arch/designpattern/hilt/MainModule.kt
+++ app/src/main/java/com/ichwan/arch/designpattern/hilt/MainModule.kt
@@ -6,15 +6,17 @@ import dagger.Module
 import dagger.Provides
 import dagger.hilt.InstallIn
 import dagger.hilt.android.components.ActivityComponent
+import dagger.hilt.android.components.ActivityRetainedComponent
 import dagger.hilt.android.qualifiers.ApplicationContext
+import dagger.hilt.android.scopes.ActivityRetainedScoped
 import dagger.hilt.android.scopes.ActivityScoped
 import javax.inject.Named
 
 @Module
-@InstallIn(ActivityComponent::class)
+@InstallIn(ActivityRetainedComponent::class)
 object MainModule {
 
-    @ActivityScoped
+    @ActivityRetainedScoped
     @Provides
     @Named("string2")
     fun provideString2(
