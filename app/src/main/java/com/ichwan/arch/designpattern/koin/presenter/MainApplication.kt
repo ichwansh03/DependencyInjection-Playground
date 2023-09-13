663c4fb2cc35bc7135ade92c01439ff02a6381f6
ichwansh03 <ichwansholihin03@gmail.com>
2023-09-13 07:37:30 +0700

    chore: declare UserRepository

--- app/src/main/java/com/ichwan/arch/designpattern/koin/presenter/MainApplication.kt
+++ app/src/main/java/com/ichwan/arch/designpattern/koin/presenter/MainApplication.kt
@@ -1,12 +1,17 @@
 package com.ichwan.arch.designpattern.koin.presenter
 
 import android.app.Application
+import com.ichwan.arch.designpattern.koin.data.DefaultData
+import com.ichwan.arch.designpattern.koin.data.UserRepository
 import com.ichwan.arch.designpattern.koin.di.appModule
+import org.koin.android.ext.android.inject
 import org.koin.android.ext.koin.androidContext
 import org.koin.android.ext.koin.androidLogger
 import org.koin.core.context.startKoin
 
 class MainApplication : Application() {
+
+    private val userRepository : UserRepository by inject()
 
     override fun onCreate() {
         super.onCreate()
@@ -16,5 +21,8 @@ class MainApplication : Application() {
             androidContext(this@MainApplication)
             modules(appModule)
         }
+
+        userRepository.addUser(DefaultData.DEFAULT_USERS)
     }
+
 }
