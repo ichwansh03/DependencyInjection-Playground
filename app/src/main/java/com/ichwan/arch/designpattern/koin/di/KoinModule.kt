292207d3ada5b1c5f72bff983bfbb67d090ba6b5
ichwansh03 <ichwansholihin03@gmail.com>
2023-09-13 07:38:00 +0700

    fixed: changed from DSL to constructor

--- app/src/main/java/com/ichwan/arch/designpattern/koin/di/KoinModule.kt
+++ app/src/main/java/com/ichwan/arch/designpattern/koin/di/KoinModule.kt
@@ -3,10 +3,19 @@ package com.ichwan.arch.designpattern.koin.di
 import com.ichwan.arch.designpattern.koin.data.UserRepository
 import com.ichwan.arch.designpattern.koin.data.UserRepositoryImpl
 import com.ichwan.arch.designpattern.koin.presenter.UserPresenter
+import com.ichwan.arch.designpattern.koin.presenter.UserViewModel
+import org.koin.androidx.viewmodel.dsl.viewModelOf
+import org.koin.core.module.dsl.bind
+import org.koin.core.module.dsl.factoryOf
+import org.koin.core.module.dsl.singleOf
 import org.koin.dsl.module
 
 val appModule = module {
-    single<UserRepository> { UserRepositoryImpl() }
-    factory { UserPresenter(get()) }
+//    single<UserRepository> { UserRepositoryImpl() }
+//    factory { UserPresenter(get()) }
+//    viewModel { UserViewModel(get()) }
 
+    singleOf(::UserRepositoryImpl) { bind<UserRepository>() }
+    factoryOf(::UserPresenter)
+    viewModelOf(::UserViewModel)
 }
